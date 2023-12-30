78ab131037bed98f7ab2b69bf07fc1198d1d283e
syc2012 <87954959+syc2012@users.noreply.github.com>
2023-12-30 15:16:09 +0800

    Update nptp_proxy.h

--- application/nptp_proxy.h
+++ application/nptp_proxy.h
@@ -2,9 +2,9 @@
 #define __NPTP_PROXY_H__
 
 
-void proxy_launchTcp(tMapping *pMapping);
+int  proxy_launchTcp(tMapping *pMapping);
 void proxy_ceaseTcp(tMapping *pMapping);
-void proxy_connectIpc(tMapping *pMapping);
+int  proxy_connectIpc(tMapping *pMapping);
 void proxy_disconnectIpc(tMapping *pMapping);
 int  proxy_init(void);
 void proxy_uninit(void);
